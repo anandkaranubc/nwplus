b9452132e844efea2a96917cb5377063f344f7f0
Karan Anand <anandkarancompsci@gmail.com>
2023-01-21 14:34:36 -0800

    Involving the audio file

---
+++ NWplus project/audiototext.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+
+with sr.Microphone() as source:
+    audio = r.listen(source)
+
+
+text = r.recognize_google(audio, show_all=True)
+
+print(text)
+
+
